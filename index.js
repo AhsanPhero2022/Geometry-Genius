a8b8c7d8eed6a7c7476052eaa06aa5da11231ce7
AhsanPhero2022e <whitehat2090@gmaile.com>
2023-02-18 23:08:16 +0600

    question answer done

--- index.js
+++ index.js
@@ -175,7 +175,7 @@ function getInnerText(id) {
 function randomColor(input) {
   const firstCard = document.getElementById(input);
   firstCard.style.backgroundColor =
-    "#" + (((1 << 24) * Math.random()) | 0).toString(16).padStart(6, "0");
+    "#" + (((1 << 22) * Math.random()) | 0).toString(14).padStart(6, "0");
 }
 document
   .getElementById("first-card")
